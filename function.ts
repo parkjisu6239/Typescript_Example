d9f4ec2528405e6e54d063df8149c75719de9f42
parkjisu6239 <parkjisu6239@gmail.com>
2022-03-25 23:22:36 +0900

    선택적 매개변수와 오버로드

--- function.ts
+++ function.ts
@@ -27,3 +27,34 @@ function combine<Type>(arr1: Type[], arr2: Type[]): Type[] {
 
 const arr = combine([1, 2, 3], ["hello"]);
 const arr2 = combine<string | number>([1, 2, 3], ["hello"]);
+
+function f(x?: number) {
+    // ...
+}
+f(); // OK
+f(10); // OK
+
+function f1(x = 1) {
+	// ...
+}
+
+f1()
+f1(4)
+
+function f2(x?: number): void {
+    //
+}
+
+f2()
+f2(1)
+f2(undefined)
+
+function myForEach(arr: any[], callback: (arg: any, index?: number) => void) {
+    for (let i = 0; i < arr.length; i++) {
+      callback(arr[i], i);
+    }
+}
+
+myForEach([1, 2, 3], (a, i) => {
+    console.log(i.toFixed());
+});
